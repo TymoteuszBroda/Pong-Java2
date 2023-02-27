bda952212294bb6dfb04de19bcc849a5efd3d63c
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 11:28:44 +0100

    finished instationing Ball in GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -38,7 +38,8 @@ public class GamePanel extends JPanel implements Runnable
 
     public void newBall()
     {
-
+//        random = new Random();
+        ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),(GAME_HEIGHT/2)-(BALL_DIAMETER/2), BALL_DIAMETER, BALL_DIAMETER);
     }
     public void newPaddles()
     {
@@ -63,7 +64,7 @@ public class GamePanel extends JPanel implements Runnable
     {
         paddle1.move();
         paddle2.move();
-        ball.move();
+        //ball.move();
     }
     public void checkCollision()
     {
