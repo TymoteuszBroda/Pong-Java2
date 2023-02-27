55a62a01110678cdfb46107f693589e9ed471795
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 10:00:53 +0100

    added blank method bodies to GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,40 @@
+import java.awt.*;
+
 public class GamePanel {
     public GamePanel()
     {
 
     }
+
+    public void newBall()
+    {
+
+    }
+    public void newPaddles()
+    {
+
+    }
+    public void paint(Graphics g)
+    {
+
+    }
+    public void draw(Graphics g)
+    {
+
+    }
+    public void move()
+    {
+
+    }
+    public void checkCollision()
+    {
+
+    }
+    public void run()
+    {
+        
+    }
+
+
+
 }
