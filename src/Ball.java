431a92eeaf28373e1530c9d9478fe60062630451
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 11:32:40 +0100

    finished constructor for Ball class

--- src/Ball.java
+++ src/Ball.java
@@ -7,9 +7,22 @@ public class Ball extends Rectangle
     Random random;
     int xVelocity;
     int yVelocity;
-    public Ball()
+    public Ball(int x, int y, int width, int height)
     {
-
+        super(x,y,width,height);
+        random= new Random();
+        int randomXDirection = random.nextInt(2);
+        if (randomXDirection == 0)
+        {
+            randomXDirection--;
+        }
+        setXDirection(randomXDirection);
+        int randomYDirection = random.nextInt(2);
+        if (randomYDirection == 0)
+        {
+            randomYDirection--;
+        }
+        setYDirection(randomYDirection);
     }
     public void setXDirection(int randomXDirection)
     {
