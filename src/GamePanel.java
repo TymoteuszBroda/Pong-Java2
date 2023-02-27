c4097b6954846f6d4fac537f9f3020d3263d15ed
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 11:26:22 +0100

    fixed laggy movement of the paddles

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -61,7 +61,9 @@ public class GamePanel extends JPanel implements Runnable
     }
     public void move()
     {
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
     public void checkCollision()
     {
