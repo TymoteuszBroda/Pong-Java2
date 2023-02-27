e33b77789317038460f0949a2efa6a80391d36d7
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 10:05:50 +0100

    added blank method bodies to the Ball class

--- src/Ball.java
+++ src/Ball.java
@@ -1,6 +1,24 @@
+import java.awt.*;
+
 public class Ball {
     public Ball()
     {
 
+    }
+    public void setXDirection(int randomXDirection)
+    {
+
+    }
+    public void setYDirection(int randomYDirection)
+    {
+
+    }
+    public void move()
+    {
+
+    }
+    public void draw(Graphics g)
+    {
+        
     }
 }
