4161529c011cf28af9465bc7a0b340b1dc09aaa6
Tymoteusz Broda <tymoteusz.91100@gmail.com>
2023-02-27 11:47:14 +0100

    Added ffuncionality to Bounce ball from paddles

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -76,7 +76,41 @@ public class GamePanel extends JPanel implements Runnable
         }
         if (ball.y>=GAME_HEIGHT-BALL_DIAMETER)
         {
-            ball.setYDirection(-GAME_HEIGHT-BALL_DIAMETER);
+            ball.setYDirection(-ball.yVelocity);
+        }
+
+        //Bounces ball of the paddles
+        if (ball.intersects(paddle1))
+        {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++;
+            if (ball.yVelocity>0)
+            {
+                ball.yVelocity++;
+            }
+            else
+            {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+
+        }
+        if (ball.intersects(paddle2))
+        {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++;
+            if (ball.yVelocity>0)
+            {
+                ball.yVelocity++;
+            }
+            else
+            {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+
         }
 
 
